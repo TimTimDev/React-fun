544f8c7a0c7307af5bbb667db2f0c4f556c2f89a
TimTimDev <timo.pirn@gmail.com>
2021-12-20 13:02:39 +0100

    Created app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
+import legend from './legend.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="Images-logos" alt="logo" />
+        <img src={legend} className="Images-logos Doggo" alt="Dog behind wine glass" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          WWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!!
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
         <a
           className="App-link"
@@ -15,8 +18,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn to spin!
         </a>
+      <h1>Hello JSX!</h1>
+      <p>This ought to be interesting</p>
+      <p>Also, how tf is that icon even spinning??</p>
       </header>
     </div>
   );
